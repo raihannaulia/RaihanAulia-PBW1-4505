bcfe7400dedbff851efdb4a84dda9c00c323b276
raihannaulia <91330311+raihannaulia@users.noreply.github.com>
2022-04-09 21:41:30 +0700

    Add files via upload

---
+++ cv_php/testing.php
@@ -0,0 +1,80 @@
+<!DOCTYPE HTML>
+<html>
+    <style>
+        body{
+            background-color:lightslategrey;
+            font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif
+        }
+        .ttd{
+            font-family:Verdana;
+           
+        }
+    </style>
+    <head>
+        
+        <body>
+           <h1 align="center"class="ttd">Curriculum Vitae</h1> 
+           <hr>
+           <h2 align="center">PERSONAL INFORMATION</h2>
+           <hr>
+           <table style="width:50%">
+   
+           <table style="width:50%">
+            <img src="4x6.jpg" align="right" width="104" height="142">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>Nama Lengkap</td>
+                <td>: Raihan Aulia Rahman </td>
+            </tr>
+            <tr>
+                <td>Tempat Tanggal Lahir</td>
+                <td>: Bandung, 28 Oktober 2002 </td>   
+            </tr>
+                <td>Alamat</td>
+                <td>: Jl Dieng Blok A/9 Kodau V, Ambarapura, Jatiasih, Bekasi</td>
+            <tr>
+                <td>Status</td>
+                <td>: Mahasiswa</td>
+            </tr>
+                <tr>
+                <td>NO HP</td>
+                <td>: 0813xxxxxxxx</td>
+            </tr>
+            <tr>
+                <td>Email</td>
+                <td>: <a href="mailto:raikodau@gmail.com">raikodau@gmail.com</a></td>
+            </tr>
+            </table>
+    
+            <hr>
+            <h2 align="center">PENDIDIKAN</h2>
+            <hr>
+                <table style="width:80%">
+            <tr>
+            <th></th>
+            <th></th>
+            </tr>
+            <tr>
+                <td>SD</td>
+                <td>: SDIT ALMARJAN </td>
+            </tr>
+            <tr>
+                <td>SMP</td>
+                <td>: SMPIT ALMARJAN </td>   
+            </tr>
+                <td>SMA</td>
+                <td>: SMA FUTURE GATE</td>
+            <tr>
+            </table>
+            <hr>
+            <h2 align="center">KEAHLIAN</h2>
+            <hr>
+            Microsoft Office<br>
+            Adobe<br>
+
+        </body>
+    </head>
+</html>
